b82efde440c51ab71cb93ef179304d5c91e00818
megamilkx <gyrocopter3k@gmail.com>
2017-03-23 23:43:06 +0300

    Add Perspective() to camera component

--- src/components/camera.h
+++ src/components/camera.h
@@ -10,11 +10,22 @@
 class Camera : public SceneObject::Component
 {
 public:
+    Camera()
+    {
+        Perspective(1.6f, 16.0f/9.0f, 0.1f, 100.0f);
+    }
+    
+    void Ortho(){}
+    void Perspective(float fov, float aspect, float zNear, float zFar)
+    {
+        projection = Au::Math::Perspective(fov, aspect, zNear, zFar);
+    }
+
     void Render(Au::GFX::Device* device)
     {
         gfxScene->Render(
             device,
-            perspective,
+            projection,
             transform->GetTransform()
         );
     }
@@ -25,7 +36,7 @@ public:
         gfxScene = GetParentObject()->Root()->GetComponent<GFXScene>();
     }
 private:
-    Au::Math::Mat4f perspective;
+    Au::Math::Mat4f projection;
 
     Transform* transform;
     GFXScene* gfxScene;
