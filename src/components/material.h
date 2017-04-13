85f141a1ab8f4a66a3992e0381f0656aed7903fd
megamilkx <gyrocopter3k@gmail.com>
2017-04-13 10:31:18 +0300

    Add material component

---
+++ src/components/material.h
@@ -0,0 +1,20 @@
+#ifndef COMPONENT_MATERIAL_H
+#define COMPONENT_MATERIAL_H
+
+#include "../scene_object.h"
+
+class Material : public SceneObject::Component
+{
+public:
+    virtual void OnCreate()
+    {
+        
+    }
+    
+    void Source(const std::string& source)
+    {
+        
+    }
+};
+
+#endif
