126ecbb8cc3a686e1a4c08e006f36dd37b51050e
megamilkx <gyrocopter3k@gmail.com>
2017-04-24 02:23:41 +0300

    Update scene script

--- build/data/scene.lua
+++ build/data/scene.lua
@@ -6,13 +6,17 @@ Init = function()
     
     light = SceneObject:CreateObject():LightOmni();
     light:Color(0.8, 0.4, 1.0);
-    Print("Calling GetObject()");
-    o = light:GetObject();Print("GetObject() done");
+    
+    o = light:GetObject();
     o:Transform():Position(-0.5, 1.7, 0.5);
     
     ld = SceneObject:CreateObject():LightDirect();
     ld:Color(0.8, 0.6, 0.4);
     ld:Direction(0.0, -1.0, 0.0);
+    
+    ld = SceneObject:CreateObject():LightDirect();
+    ld:Color(0.4, 0.3, 0.2);
+    ld:Direction(0.0, -0.5, -0.5);
     
     mesh = SceneObject:CreateObject():Mesh();
     mesh:SetMesh("miku");
@@ -22,6 +26,10 @@ Init = function()
     teapot:SetMesh("teapot");
     teapot:SetMaterial("material1");
     teapot:GetObject():Transform():Translate(-2.5, 0.0, 1.5);
+    
+    mesh = SceneObject:CreateObject():Mesh();
+    mesh:SetMesh("env");
+    mesh:SetMaterial("material");
     
     Print("Done");
 end
