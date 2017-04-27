9d34c7006a33352ed509e248ae3fac0a2228d796
megamilkx <gyrocopter3k@gmail.com>
2017-04-27 10:22:58 +0300

    Add unfinished animation component

---
+++ src/components/animation.h
@@ -0,0 +1,50 @@
+#ifndef ANIMATION_H
+#define ANIMATION_H
+
+#include <fstream>
+
+#include <aurora/media/fbx.h>
+
+#include "../scene_object.h"
+
+class AnimData
+{
+    
+};
+
+struct AnimDataReaderFBX : public Resource<AnimData>::Reader
+{
+    AnimData* operator()(const std::string& filename)
+    {
+        std::ifstream file(filename, std::ios::binary | std::ios::ate);
+        std::streamsize size = file.tellg();
+        file.seekg(0, std::ios::beg);
+        std::vector<char> buffer((unsigned int)size);
+        if(file.read(buffer.data(), size))
+        {            
+            Au::Media::FBX::Reader fbxReader;
+            fbxReader.ReadFile(buffer.data(), buffer.size());
+            fbxReader.ConvertCoordSys(Au::Media::FBX::OPENGL);
+            
+            
+        }
+        
+        return 0;
+    }
+};
+
+class Animation : public SceneObject::Component
+{
+public:
+    void SetAnimData(const std::string& name)
+    {
+        
+    }
+    
+    virtual void OnCreate()
+    {
+        
+    }
+};
+
+#endif
