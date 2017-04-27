5cdc4366ea7a42d63a9f3ea57a23265bdfd5a1ac
megamilkx <gyrocopter3k@gmail.com>
2017-04-28 01:34:22 +0300

    Add sphere script

---
+++ build/data/sphere.lua
@@ -0,0 +1,14 @@
+
+Init = function()
+    --mesh = SceneObject:Mesh();
+    --mesh:SetMesh("sphere");
+    --mesh:SetMaterial("material1");
+    --SceneObject:Transform():Translate(2.5, 0.0, 1.5);
+    
+    anim = SceneObject:Animation();
+    anim:SetAnimData("skin");
+end
+
+Update = function()
+    --SceneObject:Transform():Rotate(0.001, 0.0, 1.0, 0.0);
+end
