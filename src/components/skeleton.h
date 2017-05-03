3d27e949048fc6edf31d47d3d77f1f1364ef2c39
megamilkx <gyrocopter3k@gmail.com>
2017-05-03 03:10:26 +0300

    Add skeleton building from SkeletonData

--- src/components/skeleton.h
+++ src/components/skeleton.h
@@ -3,9 +3,22 @@
 
 #include "animation.h"
 
+struct BoneData
+{
+    ~BoneData()
+    { for(unsigned i = 0; i < children.size(); ++i) delete children[i]; }
+    
+    std::string name;
+    std::string parentName;
+    Au::Math::Mat4f transform;
+    std::vector<BoneData*> children;
+};
+
 struct SkeletonData
 {
-    
+    ~SkeletonData()
+    { for(unsigned i = 0; i < rootBones.size(); ++i) delete rootBones[i]; }
+    std::vector<BoneData*> rootBones;
 };
 
 struct SkeletonDataReaderFBX : public Resource<SkeletonData>::Reader
@@ -26,11 +39,67 @@ struct SkeletonDataReaderFBX : public Resource<SkeletonData>::Reader
             fbxReader.ReadFile(buffer.data(), buffer.size());
             fbxReader.ConvertCoordSys(Au::Media::FBX::OPENGL);
             
-            fbxReader.GetBones();
+            std::vector<Au::Media::FBX::Bone> bones = 
+                fbxReader.GetBones();
+            
+            for(unsigned i = 0; i < bones.size(); ++i)
+            {
+                BoneData* bd = new BoneData();
+                bd->name = bones[i].name;
+                bd->transform = bones[i].transform;
+                Au::Media::FBX::Bone* parent = GetBoneByUID(bones, bones[i].puid);
+                if(parent) bd->parentName = parent->name;
+                skel->rootBones.push_back(bd);
+            }
+            
+            MakeBoneTree(skel);
+            
             //fbxReader.GetHelpers();
         }
         
         return skel;
+    }
+    
+    void MakeBoneTree(SkeletonData* skel)
+    {
+        std::vector<BoneData*>& bones = 
+            skel->rootBones;
+        std::vector<BoneData*> rootBones;
+        std::vector<unsigned> rootBoneIndices;
+        for(unsigned i = 0; i < bones.size(); ++i)
+        {
+            BoneData* parent = GetBoneData(skel, bones[i]->parentName);
+            if(parent)
+                parent->children.push_back(bones[i]);
+            else
+                rootBoneIndices.push_back(i);
+        }
+        
+        for(unsigned i = 0; i < rootBoneIndices.size(); ++i)
+            rootBones.push_back(bones[rootBoneIndices[i]]);
+        bones = rootBones;
+    }
+    
+    BoneData* GetBoneData(SkeletonData* skel, const std::string& name)
+    {
+        std::vector<BoneData*>& bones = 
+            skel->rootBones;
+        for(unsigned i = 0; i < bones.size(); ++i)
+        {
+            if(bones[i]->name == name)
+                return bones[i];
+        }
+        return 0;
+    }
+    
+    Au::Media::FBX::Bone* GetBoneByUID(std::vector<Au::Media::FBX::Bone>& bones, int64_t uid)
+    {
+        for(unsigned i = 0; i < bones.size(); ++i)
+        {
+            if(bones[i].uid == uid)
+                return &bones[i];
+        }
+        return 0;
     }
 };
 
@@ -45,6 +114,11 @@ public:
     void SetData(SkeletonData* data)
     {
         skelData = data;
+        for(unsigned i = 0; i < skelData->rootBones.size(); ++i)
+        {
+            BoneData* bone = skelData->rootBones[i];
+            CreateBone(bone, GetObject());
+        }
     }
 
     virtual void OnCreate()
@@ -52,6 +126,26 @@ public:
         
     }
 private:
+    void CreateBone(BoneData* bone, SceneObject* parentObject)
+    {
+        SceneObject* boneObject = parentObject->FindObject(bone->name);
+        if(!boneObject)
+        {
+            boneObject = parentObject->CreateObject();
+            boneObject->Name(bone->name);
+        }
+        
+        Transform* boneTransform = boneObject->GetComponent<Transform>();
+        boneTransform->SetTransform(bone->transform);
+        boneTransform->AttachTo(parentObject->GetComponent<Transform>());
+        
+        for(unsigned i = 0; i < bone->children.size(); ++i)
+        {
+            BoneData* boneData = bone->children[i];
+            CreateBone(boneData, boneObject);
+        }
+    }
+
     SkeletonData* skelData;
 };
 
