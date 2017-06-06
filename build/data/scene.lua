6d870fd71e81cdffdc73eb90dcf9264f69c6f04b
megamilkx <gyrocopter3k@gmail.com>
2017-06-06 05:01:59 +0300

    Update test scripts

--- build/data/scene.lua
+++ build/data/scene.lua
@@ -4,6 +4,18 @@ teapot = {};
 Init = function()
     Print("Executing scene script");
     
+    mesh = SceneObject:CreateObject():Mesh();
+    mesh:SetMesh("miku");
+    mesh:SetMaterial("material");
+    
+    SceneObject:CreateObject():Script():SetScript("teapot");
+    SceneObject:CreateObject():Script():SetScript("sphere");
+    SceneObject:CreateObject():Script():SetScript("sphere2");
+    
+    --mesh = SceneObject:CreateObject():Mesh();
+    --mesh:SetMesh("env");
+    --mesh:SetMaterial("material");
+    
     light = SceneObject:CreateObject():LightOmni();
     light:Color(0.8, 0.4, 1.0);
     
@@ -17,18 +29,6 @@ Init = function()
     ld = SceneObject:CreateObject():LightDirect();
     ld:Color(0.4, 0.3, 0.2);
     ld:Direction(0.0, -0.5, -0.5);
-    
-    mesh = SceneObject:CreateObject():Mesh();
-    mesh:SetMesh("miku");
-    mesh:SetMaterial("material");
-    
-    SceneObject:CreateObject():Script():SetScript("teapot");
-    SceneObject:CreateObject():Script():SetScript("sphere");
-    SceneObject:CreateObject():Script():SetScript("sphere2");
-    
-    --mesh = SceneObject:CreateObject():Mesh();
-    --mesh:SetMesh("env");
-    --mesh:SetMaterial("material");
     
     Print("Done");
 end
