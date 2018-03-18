2e6598fc931c20e6b642eecbe8d152e09bb4b7cd
megamilkx <gyrocopter3k@gmail.com>
2018-03-18 23:15:35 +0300

    Dontcare

--- src/components/gui/gui_box.h
+++ src/components/gui/gui_box.h
@@ -3,31 +3,49 @@
 
 #include "gui_root.h"
 #include "../overlay/quad.h"
+#include "../overlay/text2d.h"
+#include <functional>
 
-class GuiBox : public GuiListenerBase
+class GuiBox : public SceneObject::Component
 {
 public:
-    void OnMouseEnter(const eMouseMove* e)
+    void OnMouseEnter()
     {
-        Get<Quad>()->SetColor(1.0f, 0.0f, 0.0f, 1.0f);
+        //Get<Quad>()->SetColor(0.4f, 0.0f, 0.0f, 1.0f);
     }
     void OnMouseLeave()
     {
-        Get<Quad>()->SetColor(1.0f, 1.0f, 1.0f, 1.0f);
+        //Get<Quad>()->SetColor(0.4f, 0.4f, 0.4f, 1.0f);
     }
 
-    ~GuiBox()
-    {
-        Object()->Root()->Get<GuiRoot>()->RemoveListener(this);
-    }
     void OnCreate()
     {
-        GuiListenerBase::OnCreate();
+        Get<GuiListener>()->onMouseEnter = std::bind(&GuiBox::OnMouseEnter, this);
+        Get<GuiListener>()->onMouseLeave = std::bind(&GuiBox::OnMouseLeave, this);
 
-        Object()->Root()->Get<GuiRoot>()->AddListener(this);
+        SceneObject* titleBar = Object()->CreateObject();
+        titleBar->Get<Quad>()->quad.height = 25;
+        titleBar->Get<Quad>()->quad.width = 300;
+        titleBar->Get<Quad>()->quad.color = { 0.4f, 0.4f, 0.4f, 1.0f };
+        titleBar->Get<Transform>()->AttachTo(Get<Transform>());
+
+        SceneObject* clientArea = Object()->CreateObject();
+        clientArea->Get<Quad>()->quad.height = 500;
+        clientArea->Get<Quad>()->quad.width = 300;
+        clientArea->Get<Quad>()->quad.color = { 0.2f, 0.2f, 0.2f, 1.0f };
+        clientArea->Get<Transform>()->AttachTo(Get<Transform>());
+        clientArea->Get<Transform>()->Translate(0.0f, 25.0f, 0.0f);
+
+        SceneObject* title = Object()->CreateObject();
+        title->Get<Text2d>()->SetText("Window");
+        title->Get<Text2d>()->SetSize(14);
+        title->Get<Transform>()->AttachTo(Get<Transform>());
+        title->Get<Transform>()->Translate(5.0f, 0.0f, 0.0f);
+/*
         Quad* q = Get<Quad>();
-        q->SetImage("25544144");
+        //q->SetImage("25544144");
         q->SetSize(100, 100);
+        q->SetColor(0.4f, 0.4f, 0.4f, 1.0f);*/
     }
 };
 
