30cfa41338a002f1f2232128c403feffc57346d9
megamilkx <gyrocopter3k@gmail.com>
2018-03-26 04:04:22 +0300

    Update test data

--- build/data/script/scene.lua
+++ build/data/script/scene.lua
@@ -8,29 +8,29 @@ Init = function()
 	--floorPlane:PlaneCollider();
 	--floorPlane:PlaneCollider():Height(0.0f);
     
-    model = SceneObject:CreateObject():Model();
-    model.mesh:Set("miku2");
-    model.material:Set("material");
-    model:GetObject():SetName("MIKU");
+    --model = SceneObject:CreateObject():Model();
+    --model.mesh:Set("miku2");
+    --model.material:Set("material");
+    --model:GetObject():SetName("MIKU");
     
     camera = SceneObject:CreateObject();
     cube = SceneObject:CreateObject();
     --cube:Script():SetScript("cube");
     --camera:Script():SetScript("camera");
     
-    SceneObject:CreateObject():Script():SetScript("teapot");
-    SceneObject:CreateObject():Script():SetScript("sphere");
-    SceneObject:CreateObject():Script():SetScript("brick");
-    SceneObject:CreateObject():Script():SetScript("sphere2");
+    --SceneObject:CreateObject():Script():SetScript("teapot");
+    --SceneObject:CreateObject():Script():SetScript("sphere");
+    --SceneObject:CreateObject():Script():SetScript("brick");
+    --SceneObject:CreateObject():Script():SetScript("sphere2");
     
     model = SceneObject:CreateObject():Model();
-    model.mesh:Set("env");
+    model.mesh:Set("env2");
     model.material:Set("env");
     --model:GetObject():Transform():Scale(0.1);
-    model:GetObject():MeshCollider():SetMesh("env");
+    model:GetObject():MeshCollider():SetMesh("env2");
     
     lightHinge = SceneObject:CreateObject();
-    lightHinge:Transform():Translate(0.0, 2.0, 0.0);
+    lightHinge:Transform():Translate(0.0, 0.5, 0.0);
     
     lightObject = SceneObject:CreateObject();
     lightObject:Transform():AttachTo(lightHinge:Transform());
@@ -38,11 +38,11 @@ Init = function()
     lightObject:LightOmni():Color(0.8, 0.4, 1.0);
     
     ld = SceneObject:CreateObject():LightDirect();
-    ld:Color(0.4, 0.3, 0.2);
+    ld:Color(0.8, 0.6, 0.4);
     ld:Direction(0.0, -1.0, 0.0);
     
     ld = SceneObject:CreateObject():LightDirect();
-    ld:Color(0.2, 0.15, 0.1);
+    ld:Color(0.4, 0.30, 0.2);
     ld:Direction(0.0, -0.5, -0.5);
     
     Print("Done");
